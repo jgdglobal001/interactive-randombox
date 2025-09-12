6dde0191a7d7475299f1dcef34ad07736f775b43
jgdblobal001 <jgdblobal001@gmail.com>
2025-09-12 17:33:58 +0900

    표준 Next.js 빌드로 되돌림림

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingExcludes: {
-    '*': ['.next/cache/**'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        cacheGroups: {
-          framework: {
-            chunks: 'all',
-            name: 'framework',
-            test: /(?<!node_modules.*)[\\/]node_modules[\\/](react|react-dom|scheduler|prop-types|use-subscription)[\\/]/,
-            priority: 40,
-            enforce: true,
-          },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'lib',
-            priority: 30,
-          },
-        },
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
